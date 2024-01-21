6fd8c317258b066acd9d0a20e5cbcda194b9480e
to-quy <quyto113@gmail.com>
2024-01-21 14:01:53 +0700

    Create check-login.js

---
+++ backend/api/middleware/check-login.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+
+        const token = req.headers.authorization.split(" ")[1];
+        if (global.tokenBlacklist.includes(token)) {
+            return res.status(401).json({
+                message: "Phiên đăng nhập đã hết hạn. Vui lòng đăng nhập lại."
+            });
+        }
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+
+        req.userData = decoded;
+
+        next();
+    } catch (error) {
+        console.error("Authentication error:", error); 
+        return res.status(401).json({
+            message: 'Bạn chưa đăng nhập.'
+        });
+    }
+};
